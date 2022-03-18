248b6d481b03a662cf077c777fc248120d329f2d
whatanakKean <85047164+whatanakKean@users.noreply.github.com>
2022-03-18 22:02:30 +0700

    Add files via upload

---
+++ QuadraticEquationWindow/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace QuadraticEquationWindow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.result2 = new System.Windows.Forms.TextBox();
+            this.result1 = new System.Windows.Forms.TextBox();
+            this.inputC = new System.Windows.Forms.TextBox();
+            this.inputB = new System.Windows.Forms.TextBox();
+            this.inputA = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(578, 90);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(148, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quadratic Equation Calculator";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // panel3
+            // 
+            this.panel3.AutoSize = true;
+            this.panel3.Controls.Add(this.btnClear);
+            this.panel3.Controls.Add(this.btnCalculate);
+            this.panel3.Controls.Add(this.result2);
+            this.panel3.Controls.Add(this.result1);
+            this.panel3.Controls.Add(this.inputC);
+            this.panel3.Controls.Add(this.inputB);
+            this.panel3.Controls.Add(this.inputA);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel3.Location = new System.Drawing.Point(0, 90);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(579, 321);
+            this.panel3.TabIndex = 0;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(310, 261);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 30);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(408, 261);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(94, 30);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // result2
+            // 
+            this.result2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result2.Location = new System.Drawing.Point(204, 211);
+            this.result2.Name = "result2";
+            this.result2.ReadOnly = true;
+            this.result2.Size = new System.Drawing.Size(243, 26);
+            this.result2.TabIndex = 6;
+            this.result2.TextChanged += new System.EventHandler(this.result_TextChanged);
+            // 
+            // result1
+            // 
+            this.result1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result1.Location = new System.Drawing.Point(204, 175);
+            this.result1.Name = "result1";
+            this.result1.ReadOnly = true;
+            this.result1.Size = new System.Drawing.Size(243, 26);
+            this.result1.TabIndex = 5;
+            this.result1.TextChanged += new System.EventHandler(this.result_TextChanged);
+            // 
+            // inputC
+            // 
+            this.inputC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputC.Location = new System.Drawing.Point(204, 119);
+            this.inputC.Name = "inputC";
+            this.inputC.Size = new System.Drawing.Size(243, 26);
+            this.inputC.TabIndex = 2;
+            this.inputC.TextChanged += new System.EventHandler(this.inputC_TextChanged);
+            // 
+            // inputB
+            // 
+            this.inputB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputB.Location = new System.Drawing.Point(204, 69);
+            this.inputB.Name = "inputB";
+            this.inputB.Size = new System.Drawing.Size(243, 26);
+            this.inputB.TabIndex = 1;
+            this.inputB.TabStop = false;
+            this.inputB.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // inputA
+            // 
+            this.inputA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputA.Location = new System.Drawing.Point(204, 23);
+            this.inputA.Name = "inputA";
+            this.inputA.Size = new System.Drawing.Size(243, 26);
+            this.inputA.TabIndex = 0;
+            this.inputA.TabStop = false;
+            this.inputA.TextChanged += new System.EventHandler(this.inputA_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(126, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Result = ";
+            this.label5.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(126, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Input C = ";
+            this.label4.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(126, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Input B = ";
+            this.label3.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(126, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Input A = ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(578, 411);
+            this.panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 411);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Quadratic Equation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox result2;
+        private System.Windows.Forms.TextBox result1;
+        private System.Windows.Forms.TextBox inputC;
+        private System.Windows.Forms.TextBox inputB;
+        private System.Windows.Forms.TextBox inputA;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ QuadraticEquationWindow/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuadraticEquationWindow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b, c, delta;
+            double result1, result2;
+            
+
+            if(!int.TryParse(inputA.Text, out a)) {
+                MessageBox.Show("Wrong first input!", "Input A");
+                inputA.Clear();
+                inputA.Focus();
+                return;
+            }
+            a = int.Parse(inputA.Text);
+            
+            if (!int.TryParse(inputB.Text, out b))
+            {
+                MessageBox.Show("Wrong first input!", "Input B");
+                inputB.Clear();
+                inputB.Focus();
+                return;
+            }
+            b = int.Parse(inputB.Text);
+
+            if (!int.TryParse(inputC.Text, out c))
+            {
+                MessageBox.Show("Wrong first input!", "Input C");
+                inputC.Clear();
+                inputC.Focus();
+                return;
+            }
+            c = int.Parse(inputC.Text);
+
+            delta = (b * b) - (4 * a * c);
+
+            if (delta > 0)
+            {
+                result1 = (double)((-b) + Math.Sqrt(delta)) / (2 * a);
+                result2 = (double)((-b) - Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine("Result = " + result1 + ", " + result2);
+
+                this.result1.Text = result1.ToString();
+                this.result2.Text = result2.ToString();
+            }
+            else if (delta < 0)
+            {
+                double realNumberPart = (double)(-b) / (2 * a);
+                double imaginaryPart = (double)Math.Sqrt(-1 * delta) / (2 * a);
+
+                this.result1.Text = realNumberPart + " + " + imaginaryPart + "i ";
+                this.result2.Text = realNumberPart + " - " + imaginaryPart + "i ";
+            }
+            else if (delta == 0)
+            {
+                result1 = (double)(-1 * b) / (2 * a);
+                Console.WriteLine("Result = " + result1);
+
+                this.result1.Text = result1.ToString();
+            }
+
+            
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inputA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void result_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            inputA.Clear();
+            inputB.Clear();
+            inputC.Clear();
+            result1.Clear();
+            result2.Clear();
+        }
+
+        private void inputC_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
